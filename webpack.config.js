23d7478b824a1637dc8c180eda840f0e8dbd9d89
raywanggg <784021745@qq.com>
2017-03-07 09:30:15 +0800

    webpack设置

---
+++ webpack.config.js
@@ -0,0 +1,114 @@
+var webpack = require("webpack");
+var path = require('path');
+var publicPath = "./build/";
+// var ExtractTextPlugin = require("extract-text-webpack-plugin");
+// var cssLoader = ExtractTextPlugin.extract("style-loader", "css-loader?sourceMap!postcss-loader");
+// var cssLoader = ExtractTextPlugin.extract({
+//     fallbackLoader: 'style-loader',
+//     loader: [
+//         {loader: 'css-loader', query: {sourceMap: true}},
+//         {loader: 'postcss-loader'}
+//     ]
+// });
+// var cssLoader = ExtractTextPlugin.extract('style-loader', 'css-loader', 'postcss-loader');
+
+
+// 路由映射
+// var proxy = {
+//     "/api/*": { target: "http://python-china.org", host: "python-china.org", secure: false, changeOrigin: true },
+//     "/session*": { target: "http://python-china.org", host: "python-china.org", secure: false, changeOrigin: true }
+// };
+
+
+module.exports = {
+    entry: {
+        app: "./src/index.js"
+    },
+
+    output: {
+        // path: __dirname + publicPath,
+        path: "./build",
+        filename: 'app.js',
+        publicPath: publicPath,
+        devtoolModuleFilenameTemplate: function(info) {
+            return info.resource;
+        }
+    },
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            loader: 'vue-loader',
+            options: {
+                postcss: [
+                    require('postcss-import'),
+                    require('postcss-url'),
+                    require('postcss-mixins'),
+                    require('postcss-nested'),
+                    require('postcss-css-variables'),
+                    require('autoprefixer')
+                ]
+            }
+        }, {
+            test: /\.css$/,
+            use: [{
+                loader: 'style-loader'
+            }, {
+                loader: 'css-loader'
+            }, {
+                loader: 'postcss-loader',
+                options: {
+                    plugins: function() {
+                        return [
+                            require('postcss-import'),
+                            require('postcss-url'),
+                            require('postcss-mixins'),
+                            require('postcss-nested'),
+                            require('postcss-css-variables'),
+                            require('autoprefixer')
+                        ]
+                    }
+                }
+            }]
+        }, {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015'],
+                plugins: ['transform-runtime']
+            }
+        }, {
+            test: /\.(jpg|png|gif)$/,
+            loader: 'url-loader?limit=8192'
+        }, {
+            test: /\.(eot|svg|ttf|woff|woff2)$/,
+            loader: 'url-loader?limit=10000'
+        }]
+
+    },
+    devServer: {
+        historyApiFallback: true,
+        host: '0.0.0.0',
+        // proxy: proxy,
+        port: 7070
+    },
+
+    devtool: "source-map",
+    // plugins: [
+    //     new ExtractTextPlugin('app.css'),
+    //     new webpack.LoaderOptionsPlugin({
+    //         vue: {
+    //             postcss: [require('postcss-cssnext')()]
+    //         }
+    //     })
+    // ],
+    resolve: {
+        extensions: ['.js', '.vue', '.css'],
+        alias: {
+            vue: 'vue/dist/vue.js',
+            style: path.resolve(__dirname, './css'),
+            images: path.resolve(__dirname, './css/images'),
+            fonts: path.resolve(__dirname, './css/fonts')
+        }
+    }
+}
