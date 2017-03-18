4da3e4087028c6d92ac49688ac5694ee1cea245a
raywanggg <784021745@qq.com>
2017-03-18 17:51:50 +0800

    添加home页面

---
+++ src/views/Home.vue
@@ -0,0 +1,52 @@
+<template>
+	<div id="home">
+		<search></search>
+		<content>
+			<ul>
+				<li v-bind:class="[flag == 0? active: '']" v-on:click="flag = 0"><router-link to="/recommend">推荐</router-link></li>
+				<li v-bind:class="[flag == 1? active: '']" v-on:click="flag = 1"><router-link to="/rank">排行</router-link></li>
+				<li v-bind:class="[flag == 2? active: '']" v-on:click="flag = 2"><router-link to="/person">我的</router-link></li>
+			</ul>
+		</content>
+		<router-view name="viewall"></router-view>
+	</div>
+</template>
+<script>
+import Search from "./Search.vue";
+export default {
+	data: function() {
+		return {
+			flag: 0,
+			active: "active"//data到类的映射
+		}
+	},
+	components: {
+		Search
+	}
+}
+</script>
+<style>
+	@import "../../css/common";
+	ul {
+		margin-top: 0;
+		width: 100%;
+		height: 40px;
+		line-height: 40px;
+		li {
+			float: left;
+			cursor: pointer;
+			font-size: 18px;
+			margin: 0 10px;
+			width: calc(33% - 20px);
+			text-align: center;
+			border-bottom: 2px solid transparent;
+		}
+		.active {
+			color: #ea6f5a;
+			border-bottom-color: #ea6f5a;
+		}
+		a {
+			color: #ea6f5a;
+		}
+	}
+</style>
---
+++ src/views/Search.vue
@@ -0,0 +1,43 @@
+<template>
+	<div id="search">
+		<div class="search-wrapper">
+			<input id="searchInput" type="text" v-model="value" placeholder="输入歌曲名、歌手名或专辑名">
+			<label for="searchInput">搜索</label>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	name: "search",
+	data: function() {
+		return {
+			value: ""
+		}
+	}
+}
+</script>
+<style>
+	@import "../../css/common";
+	#search {
+		width: 100%;
+		height: 70px;
+		background: #ea6f5a;
+		.search-wrapper {
+			input {
+				width: 70%;
+			    height: 38px;
+			    margin: 15px 20px;
+			    border: 1px solid #ddd;
+			    border-radius: 20px;
+			    outline: none;
+			    text-indent: 15px;
+			}
+			label {
+				color: #fff;
+				font-size: 20px;
+				margin-left: 10px;
+				cursor: pointer;
+			}
+		}
+	}
+</style>
