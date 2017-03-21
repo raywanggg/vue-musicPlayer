e7752257938b099e6913d55e70d9cca8e1e8593e
VieZhong <zyw13579@qq.com>
2017-03-21 22:08:06 +0800

    new interface

--- server.js
+++ server.js
@@ -13,17 +13,70 @@ var app = new Koa();
 app.use(logger());
 // app.use(session(app));
 
-// app.use(staticServe('./static/'));
+app.use(staticServe('./src/assets/'));
 
 router
-    .get('/api/v1/songs', function* (next) {
-        var x = yield fs.readFile(`./database/songs_list.js`, 'utf8');
-        console.log(x);
-        this.body = JSON.parse(yield fs.readFile(`./database/songs_list.js`, 'utf8'));
-        yield next;
+    .get('/api/v1/songs/recommend', function* (next) {
+        let size = parseInt(this.request.query.size) || 8;
+        let songs = JSON.parse(yield fs.readFile(`./database/songs_list.js`, 'utf8'));
+        let result = new Set();
+        while(result.size < size) {
+            let rand = Math.floor(Math.random(0, 1) * songs.length);
+            result.add(songs[rand]);
+        }
+        this.body = Array.from(result);
+        // yield next;
+    })
+    .get('/api/v1/songs/rank', function* (next) {
+        let size = parseInt(this.request.query.size) || 8;
+        let page = parseInt(this.request.query.page) || 1;
+        let songs = JSON.parse(yield fs.readFile(`./database/songs_list.js`, 'utf8')).sort((a, b) => b.liked - a.liked);
+        let rstSongs = songs.slice(page * size - size, page * size);
+        let result = {
+            data: rstSongs,
+            totalNum: songs.length,
+            page: page,
+            size: rstSongs.length
+        };
+        this.body = result;
+        // yield next;
+    })
+    .get('/api/v1/songs/mine', function* (next) {
+        let size = parseInt(this.request.query.size) || 8;
+        let page = parseInt(this.request.query.page) || 1;
+        let songs = JSON.parse(yield fs.readFile(`./database/songs_list.js`, 'utf8'));
+        let rstSongs = songs.slice(page * size - size, page * size);
+        let result = {
+            data: rstSongs,
+            totalNum: songs.length,
+            page: page,
+            size: rstSongs.length
+        };
+        this.body = result;
+        // yield next;
+    })
+    .get('/api/v1/songs/search', function* (next) {
+        let key = this.request.query.key;
+        if(!key){
+            this.body = {data: []};
+            return;
+        }
+        let keyReg = new RegExp(key);
+        let size = parseInt(this.request.query.size) || 8;
+        let page = parseInt(this.request.query.page) || 1;
+        let songs = JSON.parse(yield fs.readFile(`./database/songs_list.js`, 'utf8'));
+        songs = songs.filter(song => keyReg.test('' + song.name + song.author));
+        let rstSongs = songs.slice(page * size - size, page * size);
+        let result = {
+            data: rstSongs,
+            totalNum: songs.length,
+            page: page,
+            size: rstSongs.length
+        };
+        this.body = result;
+        // yield next;
     })
     .get('/api/v1/songs/:id', function* (next) {
-        console.log(this.params.id);
         let songsList = JSON.parse(yield fs.readFile(`./database/songs_list.js`, 'utf8'));
         let result = songsList.find((song) => this.params.id == song.id);
         if(!result){
@@ -32,7 +85,7 @@ router
             return;
         }
         this.body = result;
-        yield next;
+        // yield next;
     });
 
 app
